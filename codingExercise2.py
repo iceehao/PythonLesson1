8f988d2e38d383818a1941b1e8a50e622aab8e85
Hao Tran <icehao1999@gmail.com>
2021-07-01 13:48:51 -0700

    first commit

---
+++ codingExercise2.py
@@ -0,0 +1,8 @@
+#you will be provided a random number in a variable called num.
+#Use a conditional statement to check if the number is odd. If num is odd, print "odd". Otherwise print"even" 
+# hint use modulus "%" to figure out if the number is odd!.
+
+# NO TOUCHING ======================================
+from random import randint
+num = randint(1, 1000) #picks random number from 1-1000
+# NO TOUCHING ======================================
